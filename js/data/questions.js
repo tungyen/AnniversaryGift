1e9ce50981896ad26b5ff84026961cbb9781b911
Tungyen <cony998820@gmail.com>
2026-07-18 17:47:40 -0700

    Update question

--- js/data/questions.js
+++ js/data/questions.js
@@ -1,7 +1,7 @@
 export const questions = [
     {
         id: 1,
-        question: "我們第一次見面是在哪裡？",
+        question: "妳是第一次在哪裡見到妳寶貝呢？",
         options: [
             "中壢夜市",
             "某家咖啡廳",
@@ -12,13 +12,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_01.jpg",
             title: "第一次見面之前我們是如何了解彼此呢?",
-            text: "我們畫了彼此的自畫像❤️"
+            text: "我們畫了彼此的自畫像，我記得我畫的第一個版本被妳嫌棄到爆哈哈哈"
         }
     },
 
     {
         id: 2,
-        question: "我們第二次約會去了哪裡呢？",
+        question: "妳看到我滿臉鬍子之後還答應我第二次出來，我們去哪約會呢？",
         options: [
             "電影院看鬼片",
             "一起畫畫",
@@ -28,14 +28,14 @@ export const questions = [
         answer: 0,
         memory: {
             image: "assets/memories/memory_02.jpg",
-            title: "你第一次幫我拍照",
-            text: "或許照片中的笑容是因為看到你可愛的模樣❤️"
+            title: "好像是妳主動說要幫我拍照",
+            text: "照片中的笑容是因為看到你可愛的樣子而心跳不已❤️"
         }
     },
 
     {
         id: 3,
-        question: "我們第一次合照是在哪裡呢？",
+        question: "我何德何能可以和妳這樣的美女拍照？我們第一張合照在哪拍的呢？",
         options: [
             "巨城",
             "尚順百貨公司",
@@ -45,14 +45,14 @@ export const questions = [
         answer: 2,
         memory: {
             image: "assets/memories/memory_03.jpeg",
-            title: "我們的第一次合照",
-            text: "感覺我們都有點小靦腆~"
+            title: "我那時候手好像在抖，有點緊張",
+            text: "感覺我們都有點小靦腆，但是這張照片我回家看著偷笑了很久❤️"
         }
     },
 
     {
         id: 4,
-        question: "妳是在什麼覺得社工鼻很特別呢？",
+        question: "妳是在什麼覺得社工鼻這傢伙很特別呢？",
         options: [
             "公園跳廣場舞",
             "我說烏龜長得像我阿祖",
@@ -63,13 +63,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_04.jpeg",
             title: "我們一起在南庄老街吃的第一道食物",
-            text: "雖然最後是我吃最多哈哈哈!"
+            text: "雖然最後是我吃最多哈哈哈，然後賣桂花釀的阿姨不知道還有沒有在等我們"
         }
     },
 
     {
         id: 5,
-        question: "我們第一次拍拍貼機是甚麼時候呢？",
+        question: "一起拍拍貼機吧!妳跟妳寶貝第一次拍拍貼機是甚麼時候呢？",
         options: [
             "第二次約會的時候",
             "2025社工鼻回台灣放假",
@@ -80,13 +80,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_05.png",
             title: "可以跟愛人拍可愛的照片很幸福",
-            text: "雖然下面寫友情可貴，但是鼻鼻也是我一生的摯愛❤️"
+            text: "雖然下面寫友情可貴，但是鼻鼻也是我一生的摯愛、朋友和家人❤️"
         }
     },
 
     {
         id: 6,
-        question: "社工鼻第一次幫妳化妝是什麼造型？",
+        question: "我的寶貝竟然化妝也很有天分!妳的社工鼻第一次幫妳化妝是什麼造型？",
         options: [
             "Lady Gaga",
             "蝙蝠俠",
@@ -97,13 +97,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_06.jpeg",
             title: "社工鼻人生第一次幫別人化妝",
-            text: "看到這樣的鼻鼻，我也會像小丑一樣對你陷入瘋狂"
+            text: "看到這樣的鼻鼻，我也會像小丑一樣對你陷入瘋狂，妳就是我的全世界!"
         }
     },
 
     {
         id: 7,
-        question: "我們在台鐵的哪一個車站巧遇呢？",
+        question: "緣分是很奇妙的事，我們連搭車都可以搭到同一班，我們在台鐵的哪一個站巧遇呢？",
         options: [
             "台中",
             "桃園",
@@ -113,14 +113,14 @@ export const questions = [
         answer: 3,
         memory: {
             image: "assets/memories/memory_07.jpeg",
-            title: "看到妳火車的班次跟我一樣很驚喜",
-            text: "就迫不及待的衝去妳的車廂內找妳了❤️"
+            title: "看到妳火車的班次跟我一樣我內心很激動",
+            text: "我一直跟別人說借過，因為迫不及待的衝去妳的車廂內找妳了❤️"
         }
     },
 
     {
         id: 8,
-        question: "2024年我回去學校之前我們在哪裡道別呢?",
+        question: "還是會需要離別🥲 2024年我回去學校之前我們在哪裡道別呢?",
         options: [
             "鼻鼻的竹南租屋處",
             "工研院",
@@ -131,13 +131,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_08.jpeg",
             title: "第一次分別",
-            text: "我們一起買了項鍊，鼻鼻快把眼淚哭乾了🥲"
+            text: "我們一起買了項鍊，鼻鼻快把眼淚哭乾了🥲，雖然那時候沒有辦法保證下次見面什麼時候，但是我現在已經承諾妳回去的時間了!"
         }
     },
 
     {
         id: 9,
-        question: "我們一起慶祝生日是去哪裡吃飯呢？",
+        question: "祝妳生日快樂!我們一起慶祝生日是去哪裡吃飯呢？",
         options: [
             "莫尼尼",
             "金色三麥",
@@ -148,13 +148,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_09.jpeg",
             title: "跟鼻鼻一起塗奶油好好玩",
-            text: "而且鼻鼻還訂了一個可愛的憤怒鳥蛋糕❤️"
+            text: "而且鼻鼻還訂了一個可愛的憤怒鳥蛋糕，但是我以後不會再亂挑禮物了鼻鼻，我學起來了!"
         }
     },
 
     {
         id: 10,
-        question: "第一次一起過聖誕節在餐廳看了什麼動物？",
+        question: "Merry Chrismas!第一次一起過聖誕節在餐廳看了什麼可愛動物呢？",
         options: [
             "地上爬的猴子",
             "公雞",
@@ -165,13 +165,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_10.jpeg",
             title: "好久沒有拿出聖誕節搞笑眼鏡",
-            text: "跟鼻鼻一起過的聖誕節是最快樂的❤️"
+            text: "跟鼻鼻一起過的聖誕節是最快樂的，以前都覺得聖誕節很普通，是妳賦予了我的聖誕節活力❤️"
         }
     },
 
     {
         id: 11,
-        question: "鼻鼻最喜歡北投的哪一個食物？",
+        question: "一起去小旅行吧!鼻鼻最喜歡北投的哪一個食物？",
         options: [
             "北投紅茶",
             "樂山窄巷拉麵",
@@ -182,13 +182,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_11.jpeg",
             title: "我們的第一次小旅行",
-            text: "一起在飯店泡溫泉鴛鴦浴❤️"
+            text: "一起在飯店泡溫泉鴛鴦浴，雖然隔音有點差，然後我以後旅行結束都會邀鼻鼻一起跟我媽吃晚餐❤️"
         }
     },
 
     {
         id: 12,
-        question: "鼻鼻是為了哪個歌手所以我們一起去台中跨年？",
+        question: "Happy New Year!鼻鼻是為了哪個歌手所以我們一起去台中跨年？",
         options: [
             "弦子",
             "告五人",
@@ -199,13 +199,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_12.jpeg",
             title: "第一次一起跨年",
-            text: "等我回去我們就去買蟑螂裝怎麼樣呢?"
+            text: "等我回去我們就去買蟑螂裝怎麼樣呢?或是牙膏裝也不錯!"
         }
     },
 
     {
         id: 13,
-        question: "我們第一次去服裝店租借了什麼衣服？",
+        question: "跟鼻鼻一起穿好看的衣服是我的夢想。我們第一次去服裝店租借了什麼衣服呢？",
         options: [
             "鬼修女衣服",
             "學生制服",
@@ -216,13 +216,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_13.jpeg",
             title: "我們看著彼此穿著和服的眼神都很有愛",
-            text: "直接榮登我們的手機桌布第一代(真實合照)❤️"
+            text: "直接榮登我們的手機桌布第一代❤️，歷久不衰"
         }
     },
 
     {
         id: 14,
-        question: "在埔心牧場我們一起帶了什麼寶貝呢？",
+        question: "一起去農場看小動物吧!在埔心牧場我們一起帶了什麼寶貝出遊呢？",
         options: [
             "卡比巴拉",
             "亞伯拉罕",
@@ -233,13 +233,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_14.jpeg",
             title: "這張很像以後我們帶小孩出門的樣子",
-            text: "我還記得粉紅色那隻的尾巴會動哈哈哈"
+            text: "我還記得粉紅色那隻的尾巴會動哈哈哈，天鵝船好像都是我在踩?"
         }
     },
 
     {
         id: 15,
-        question: "鼻鼻第一次來美國找我去了哪個城市呢(英文)？",
+        question: "終於可以見面了!鼻鼻第一次來美國找我去了哪個城市呢(英文)，我有教過妳喔!",
         options: [
             "Santa Cruz",
             "Los Angeles",
@@ -250,30 +250,30 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_15.jpeg",
             title: "我們都是第一次去看金門大橋",
-            text: "幸好路人拍照技術還可以❤️"
+            text: "幸好路人拍照技術還可以，能和鼻鼻在美國最知名的地標之一合照對我來說意義非凡❤️"
         }
     },
 
     {
         id: 16,
-        question: "鼻鼻在舊金山看到了什麼一直在笑？",
+        question: "鼻鼻:哈哈哈哈哈哈哈哈哈，鼻鼻在舊金山看到了什麼一直在笑？",
         options: [
             "飯店早餐的墨西哥辣椒醬",
             "久久沒看到的社工鼻",
-            "黑人司機",
+            "黑人司機轉叮叮車",
             "在飛"
         ],
         answer: 3,
         memory: {
             image: "assets/memories/memory_16.jpeg",
             title: "鼻鼻的笑點對我來說比21世紀十大不可思議還難掌握",
-            text: "不過這個圖案是真的挺荒謬的哈哈哈"
+            text: "不過這個圖案是真的挺荒謬的哈哈哈，到現在鼻鼻想到還是會一直笑，真有趣"
         }
     },
 
     {
         id: 17,
-        question: "在踏上舊金山惡魔島之後我們必須先做什麼事？",
+        question: "這裡是關了許多重罪犯的地方，在踏上舊金山惡魔島之後我們必須先做什麼事呢？",
         options: [
             "用湯匙挖牆壁",
             "閉上我們的嘴",
@@ -283,14 +283,14 @@ export const questions = [
         answer: 1,
         memory: {
             image: "assets/memories/memory_17.jpeg",
-            title: "好像是第一次跟鼻鼻一起搭船",
-            text: "鼻鼻手機差點掉進海裡???"
+            title: "第一次跟鼻鼻一起搭船，一起吹海風",
+            text: "鼻鼻手機差點掉進海裡，差點照片就只剩下我手機拍的那些了???"
         }
     },
 
     {
         id: 18,
-        question: "鼻鼻第一次放了我拍的照片當頭貼是哪一張",
+        question: "鼻鼻你拍得很好耶!鼻鼻第一次放了我拍的照片當頭貼是哪一張",
         options: [
             "鶯歌老街",
             "紅木森林公園停車場拍的",
@@ -301,13 +301,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_18.jpeg",
             title: "看到這張照片我就決定要多帶妳去公園走走",
-            text: "愛好大自然的鼻鼻好可愛❤️"
+            text: "愛好大自然的鼻鼻好可愛，而且被認可拍照技術的時候真的很開心，被罵好幾次都沒關係，苦盡甘來❤️"
         }
     },
 
     {
         id: 19,
-        question: "我們第一次帶亞伯拉罕出門是去哪呢？",
+        question: "爸爸帶我出門辣!在家都要發霉了。我們第一次帶亞伯拉罕出門是去哪呢？",
         options: [
             "聖荷西玫瑰園",
             "99 Ranch 中國超市",
@@ -318,13 +318,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_19.jpeg",
             title: "跟鼻鼻來公園散步就算天氣再熱也幸福",
-            text: "雖然亞伯拉罕腳濕濕和手髒髒(按了過馬路按鈕)"
+            text: "雖然亞伯拉罕腳濕濕和手髒髒(按了過馬路按鈕)，但是它有吃到冰淇淋"
         }
     },
 
     {
         id: 20,
-        question: "鼻鼻第一次來找我回去的時候綁架了哪一個寶貝走？",
+        question: "我的寶貝呢?鼻鼻第一次來找我回去的時候綁架了哪一個寶貝走？",
         options: [
             "布丁",
             "黑皮",
@@ -341,7 +341,7 @@ export const questions = [
 
     {
         id: 21,
-        question: "我們的一周年旅行去哪個城市(英文)呢？",
+        question: "鼻鼻一周年請多多指教，我們的一周年旅行去哪個城市(英文)呢？",
         options: [
             "San Francisco",
             "Los Angeles",
@@ -352,13 +352,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_21.jpeg",
             title: "一起跟鼻鼻搭國內線",
-            text: "來個旅行前的四連拍吧!"
+            text: "來個旅行前的四連拍吧，我真的很喜歡你的這件衣服，好適合你，很美❤️"
         }
     },
 
     {
         id: 22,
-        question: "去羚羊谷的路上發生了什麼很荒謬的事呢？",
+        question: "哎呀來不及了，我們要遲到了鼻鼻!去羚羊谷的路上發生了什麼很荒謬的事呢？",
         options: [
             "導遊說要給我們好看",
             "被沙子噴滿臉",
@@ -369,13 +369,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_22.png",
             title: "紀錄一下鼻鼻想到用冰棒找手機的過程",
-            text: "我是生活白癡，都沒有想到有這個方法..."
+            text: "我是生活白癡，都沒有想到有這個方法，我會好好錯中學的鼻鼻"
         }
     },
 
     {
         id: 23,
-        question: "周年旅行最後一天我們去哪裡拍照呢？",
+        question: "即將跟拉斯維加斯說再見了，周年旅行最後一天我們去哪裡拍照呢？",
         options: [
             "威尼斯飯店假天景",
             "Hello Kitty咖啡廳",
@@ -386,15 +386,15 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_23.jpeg",
             title: "去的路上鼻鼻換了一套好好看的衣服",
-            text: "可愛到我想一直摸摸她的頭❤️"
+            text: "可愛到我想一直摸摸她的頭，果然鼻鼻是天生的衣架子，搭好多衣服都好好看❤️"
         }
     },
 
     {
         id: 24,
-        question: "我們2025聖誕節時在電梯做什麼事呢？",
+        question: "嘻嘻我們要不要偷偷來呢❤️我們2025聖誕節時在電梯做什麼事呢？",
         options: [
-            "發呆",
+            "偷抓屁股",
             "討論晚餐吃什麼",
             "強迫啾啾",
             "拍可愛照"
@@ -403,13 +403,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_24.jpg",
             title: "雖然是電梯但是是假的哈哈哈",
-            text: "跟第一次玩拍貼機比我們感情更好了❤️"
+            text: "跟第一次玩拍貼機比，我們感情更好了，因為我好想好想鼻鼻，好不真實的感覺❤️"
         }
     },
 
     {
         id: 25,
-        question: "我第一次送鼻鼻花是送什麼花呢？",
+        question: "我要送你99朵玫瑰花!我第一次送鼻鼻花是送什麼花呢？",
         options: [
             "玫瑰",
             "小蒼蘭",
@@ -420,13 +420,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_25.jpeg",
             title: "看到妳開心興奮的表情真的很值得",
-            text: "以後都要一直送妳花代表我對你的心意!"
+            text: "以後都要一直送妳花代表我對你的心意，希望你能慢慢感受到我的浪漫，不是大直男!"
         }
     },
 
     {
         id: 26,
-        question: "我偷偷買了什麼生日禮物然後在美國給妳讓妳很開心呢？",
+        question: "鼻鼻生日快樂!我偷偷買了什麼生日禮物然後在美國給妳讓妳很開心呢？",
         options: [
             "我的智齒",
             "小雞汁娃娃",
@@ -437,13 +437,13 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_26.jpeg",
             title: "妳看到的時候露出的表情好可愛",
-            text: "你整天一直拿著它愛不釋手哈哈哈"
+            text: "你整天一直拿著它愛不釋手哈哈哈，雖然它胸口有點紅紅的，但是它現在可以每天陪著你!"
         }
     },
 
     {
         id: 27,
-        question: "喜歡我們一起煮的生日餐嗎？",
+        question: "要一起做什麼好吃的呢?喜歡我們一起煮的生日餐嗎？",
         options: [
             "喜歡，跟愛人一起做料理很開心",
             "喜歡，但是很花時間哈哈哈",
@@ -453,14 +453,14 @@ export const questions = [
         answer: 0,
         memory: {
             image: "assets/memories/memory_27.jpeg",
-            title: "雖然我做的蛋糕味道怪怪的就是了",
-            text: "但是一起協力完成的料理充滿很多❤️"
+            title: "世界上最好吃的大餐",
+            text: "雖然我做的蛋糕味道怪怪的就是了，但是一起協力完成的料理充滿很多❤️"
         }
     },
 
     {
         id: 28,
-        question: "平常我們不能見面的時候很常做什麼事呢？",
+        question: "鼻鼻我愛你，我們要一起珍惜記錄生活!平常我們不能見面的時候很常做什麼事呢？",
         options: [
             "一起看劇",
             "視訊截圖拍照",
@@ -471,7 +471,7 @@ export const questions = [
         memory: {
             image: "assets/memories/memory_28.jpg",
             title: "我們有數不清的截圖合照",
-            text: "每一張都代表我們緊緊相連的心，我愛妳鼻鼻，兩周年快樂❤️"
+            text: "每一張都代表我們緊緊相連的心，看著這些照片都會不自覺的微笑，我愛妳鼻鼻，兩周年快樂❤️"
         }
     },
 ];
