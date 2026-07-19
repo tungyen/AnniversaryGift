262ddaca7fff6a0169ae198864ddf0f29cf98841
Tungyen <cony998820@gmail.com>
2026-07-18 17:15:21 -0700

    Add more questions.

--- js/data/questions.js
+++ js/data/questions.js
@@ -10,25 +10,468 @@ export const questions = [
         ],
         answer: 2,
         memory: {
-            image: "assets/memories/memory_01.jpeg",
-            title: "第一次見到妳",
-            text: "那一天開始，我就對張淑君小姐一見鍾情 ❤️"
+            image: "assets/memories/memory_01.jpg",
+            title: "第一次見面之前我們是如何了解彼此呢?",
+            text: "我們畫了彼此的自畫像❤️"
         }
     },
 
     {
         id: 2,
-        question: "誰先說喜歡對方？",
+        question: "我們第二次約會去了哪裡呢？",
         options: [
-            "我",
-            "妳",
-            "一起"
+            "電影院看鬼片",
+            "一起畫畫",
+            "公園散步",
+            "逛展覽"
         ],
         answer: 0,
         memory: {
-            image: "assets/memories/memory_01.jpeg",
-            title: "第一次說喜歡妳",
-            text: "那一天開始，我知道妳是我想一直陪伴的人 ❤️"
+            image: "assets/memories/memory_02.jpg",
+            title: "你第一次幫我拍照",
+            text: "或許照片中的笑容是因為看到你可愛的模樣❤️"
         }
-    }
+    },
+
+    {
+        id: 3,
+        question: "我們第一次合照是在哪裡呢？",
+        options: [
+            "巨城",
+            "尚順百貨公司",
+            "幸福公園",
+            "獅頭山公園"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_03.jpeg",
+            title: "我們的第一次合照",
+            text: "感覺我們都有點小靦腆~"
+        }
+    },
+
+    {
+        id: 4,
+        question: "妳是在什麼覺得社工鼻很特別呢？",
+        options: [
+            "公園跳廣場舞",
+            "我說烏龜長得像我阿祖",
+            "一起照奇怪鏡",
+            "講了螞蟻笑話給你聽"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_04.jpeg",
+            title: "我們一起在南庄老街吃的第一道食物",
+            text: "雖然最後是我吃最多哈哈哈!"
+        }
+    },
+
+    {
+        id: 5,
+        question: "我們第一次拍拍貼機是甚麼時候呢？",
+        options: [
+            "第二次約會的時候",
+            "2025社工鼻回台灣放假",
+            "2024的颱風天",
+            "交往的第一天"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_05.png",
+            title: "可以跟愛人拍可愛的照片很幸福",
+            text: "雖然下面寫友情可貴，但是鼻鼻也是我一生的摯愛❤️"
+        }
+    },
+
+    {
+        id: 6,
+        question: "社工鼻第一次幫妳化妝是什麼造型？",
+        options: [
+            "Lady Gaga",
+            "蝙蝠俠",
+            "緋紅女巫",
+            "小丑女"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_06.jpeg",
+            title: "社工鼻人生第一次幫別人化妝",
+            text: "看到這樣的鼻鼻，我也會像小丑一樣對你陷入瘋狂"
+        }
+    },
+
+    {
+        id: 7,
+        question: "我們在台鐵的哪一個車站巧遇呢？",
+        options: [
+            "台中",
+            "桃園",
+            "中壢",
+            "新竹"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_07.jpeg",
+            title: "看到妳火車的班次跟我一樣很驚喜",
+            text: "就迫不及待的衝去妳的車廂內找妳了❤️"
+        }
+    },
+
+    {
+        id: 8,
+        question: "2024年我回去學校之前我們在哪裡道別呢?",
+        options: [
+            "鼻鼻的竹南租屋處",
+            "工研院",
+            "新竹火車站",
+            "桃園國際機場"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_08.jpeg",
+            title: "第一次分別",
+            text: "我們一起買了項鍊，鼻鼻快把眼淚哭乾了🥲"
+        }
+    },
+
+    {
+        id: 9,
+        question: "我們一起慶祝生日是去哪裡吃飯呢？",
+        options: [
+            "莫尼尼",
+            "金色三麥",
+            "青花驕",
+            "西堤牛排"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_09.jpeg",
+            title: "跟鼻鼻一起塗奶油好好玩",
+            text: "而且鼻鼻還訂了一個可愛的憤怒鳥蛋糕❤️"
+        }
+    },
+
+    {
+        id: 10,
+        question: "第一次一起過聖誕節在餐廳看了什麼動物？",
+        options: [
+            "地上爬的猴子",
+            "公雞",
+            "兔子",
+            "狗狗"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_10.jpeg",
+            title: "好久沒有拿出聖誕節搞笑眼鏡",
+            text: "跟鼻鼻一起過的聖誕節是最快樂的❤️"
+        }
+    },
+
+    {
+        id: 11,
+        question: "鼻鼻最喜歡北投的哪一個食物？",
+        options: [
+            "北投紅茶",
+            "樂山窄巷拉麵",
+            "陳記炸雞",
+            "炸蛋蔥油餅"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_11.jpeg",
+            title: "我們的第一次小旅行",
+            text: "一起在飯店泡溫泉鴛鴦浴❤️"
+        }
+    },
+
+    {
+        id: 12,
+        question: "鼻鼻是為了哪個歌手所以我們一起去台中跨年？",
+        options: [
+            "弦子",
+            "告五人",
+            "血肉果汁機",
+            "房東的貓"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_12.jpeg",
+            title: "第一次一起跨年",
+            text: "等我回去我們就去買蟑螂裝怎麼樣呢?"
+        }
+    },
+
+    {
+        id: 13,
+        question: "我們第一次去服裝店租借了什麼衣服？",
+        options: [
+            "鬼修女衣服",
+            "學生制服",
+            "和服",
+            "韓服"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_13.jpeg",
+            title: "我們看著彼此穿著和服的眼神都很有愛",
+            text: "直接榮登我們的手機桌布第一代(真實合照)❤️"
+        }
+    },
+
+    {
+        id: 14,
+        question: "在埔心牧場我們一起帶了什麼寶貝呢？",
+        options: [
+            "卡比巴拉",
+            "亞伯拉罕",
+            "泰迪",
+            "大鼻鼻"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_14.jpeg",
+            title: "這張很像以後我們帶小孩出門的樣子",
+            text: "我還記得粉紅色那隻的尾巴會動哈哈哈"
+        }
+    },
+
+    {
+        id: 15,
+        question: "鼻鼻第一次來美國找我去了哪個城市呢(英文)？",
+        options: [
+            "Santa Cruz",
+            "Los Angeles",
+            "San Fransisco",
+            "Las Vegas"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_15.jpeg",
+            title: "我們都是第一次去看金門大橋",
+            text: "幸好路人拍照技術還可以❤️"
+        }
+    },
+
+    {
+        id: 16,
+        question: "鼻鼻在舊金山看到了什麼一直在笑？",
+        options: [
+            "飯店早餐的墨西哥辣椒醬",
+            "久久沒看到的社工鼻",
+            "黑人司機",
+            "在飛"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_16.jpeg",
+            title: "鼻鼻的笑點對我來說比21世紀十大不可思議還難掌握",
+            text: "不過這個圖案是真的挺荒謬的哈哈哈"
+        }
+    },
+
+    {
+        id: 17,
+        question: "在踏上舊金山惡魔島之後我們必須先做什麼事？",
+        options: [
+            "用湯匙挖牆壁",
+            "閉上我們的嘴",
+            "跟海鷗玩耍",
+            "戴上帥氣的墨鏡"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_17.jpeg",
+            title: "好像是第一次跟鼻鼻一起搭船",
+            text: "鼻鼻手機差點掉進海裡???"
+        }
+    },
+
+    {
+        id: 18,
+        question: "鼻鼻第一次放了我拍的照片當頭貼是哪一張",
+        options: [
+            "鶯歌老街",
+            "紅木森林公園停車場拍的",
+            "拉斯維加斯羚羊谷",
+            "舊金山金門公園"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_18.jpeg",
+            title: "看到這張照片我就決定要多帶妳去公園走走",
+            text: "愛好大自然的鼻鼻好可愛❤️"
+        }
+    },
+
+    {
+        id: 19,
+        question: "我們第一次帶亞伯拉罕出門是去哪呢？",
+        options: [
+            "聖荷西玫瑰園",
+            "99 Ranch 中國超市",
+            "Westfield百貨公司",
+            "舊金山國際機場"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_19.jpeg",
+            title: "跟鼻鼻來公園散步就算天氣再熱也幸福",
+            text: "雖然亞伯拉罕腳濕濕和手髒髒(按了過馬路按鈕)"
+        }
+    },
+
+    {
+        id: 20,
+        question: "鼻鼻第一次來找我回去的時候綁架了哪一個寶貝走？",
+        options: [
+            "布丁",
+            "黑皮",
+            "大福",
+            "雪人"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_20.jpeg",
+            title: "我從那時候到現在只看到布丁本狗一次嗚嗚嗚",
+            text: "但是看到布丁能當你的枕頭或是抱枕也欣慰，至少他有作用❤️"
+        }
+    },
+
+    {
+        id: 21,
+        question: "我們的一周年旅行去哪個城市(英文)呢？",
+        options: [
+            "San Francisco",
+            "Los Angeles",
+            "San Diego",
+            "Las Vegas"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_21.jpeg",
+            title: "一起跟鼻鼻搭國內線",
+            text: "來個旅行前的四連拍吧!"
+        }
+    },
+
+    {
+        id: 22,
+        question: "去羚羊谷的路上發生了什麼很荒謬的事呢？",
+        options: [
+            "導遊說要給我們好看",
+            "被沙子噴滿臉",
+            "鼻鼻手機掉了",
+            "大遲到"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_22.png",
+            title: "紀錄一下鼻鼻想到用冰棒找手機的過程",
+            text: "我是生活白癡，都沒有想到有這個方法..."
+        }
+    },
+
+    {
+        id: 23,
+        question: "周年旅行最後一天我們去哪裡拍照呢？",
+        options: [
+            "威尼斯飯店假天景",
+            "Hello Kitty咖啡廳",
+            "歡迎光臨拉斯維加斯招牌",
+            "百樂宮大飯店後花園"
+        ],
+        answer: 2,
+        memory: {
+            image: "assets/memories/memory_23.jpeg",
+            title: "去的路上鼻鼻換了一套好好看的衣服",
+            text: "可愛到我想一直摸摸她的頭❤️"
+        }
+    },
+
+    {
+        id: 24,
+        question: "我們2025聖誕節時在電梯做什麼事呢？",
+        options: [
+            "發呆",
+            "討論晚餐吃什麼",
+            "強迫啾啾",
+            "拍可愛照"
+        ],
+        answer: 3,
+        memory: {
+            image: "assets/memories/memory_24.jpg",
+            title: "雖然是電梯但是是假的哈哈哈",
+            text: "跟第一次玩拍貼機比我們感情更好了❤️"
+        }
+    },
+
+    {
+        id: 25,
+        question: "我第一次送鼻鼻花是送什麼花呢？",
+        options: [
+            "玫瑰",
+            "小蒼蘭",
+            "鬱金香",
+            "天堂鳥"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_25.jpeg",
+            title: "看到妳開心興奮的表情真的很值得",
+            text: "以後都要一直送妳花代表我對你的心意!"
+        }
+    },
+
+    {
+        id: 26,
+        question: "我偷偷買了什麼生日禮物然後在美國給妳讓妳很開心呢？",
+        options: [
+            "我的智齒",
+            "小雞汁娃娃",
+            "小八娃娃",
+            "吉伊卡哇滑鼠墊"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_26.jpeg",
+            title: "妳看到的時候露出的表情好可愛",
+            text: "你整天一直拿著它愛不釋手哈哈哈"
+        }
+    },
+
+    {
+        id: 27,
+        question: "喜歡我們一起煮的生日餐嗎？",
+        options: [
+            "喜歡，跟愛人一起做料理很開心",
+            "喜歡，但是很花時間哈哈哈",
+            "我比較喜歡出門吃大餐❤️",
+            "我可以看社工鼻做就好嗎~"
+        ],
+        answer: 0,
+        memory: {
+            image: "assets/memories/memory_27.jpeg",
+            title: "雖然我做的蛋糕味道怪怪的就是了",
+            text: "但是一起協力完成的料理充滿很多❤️"
+        }
+    },
+
+    {
+        id: 28,
+        question: "平常我們不能見面的時候很常做什麼事呢？",
+        options: [
+            "一起看劇",
+            "視訊截圖拍照",
+            "一起玩遊戲",
+            "一起唱歌"
+        ],
+        answer: 1,
+        memory: {
+            image: "assets/memories/memory_28.jpg",
+            title: "我們有數不清的截圖合照",
+            text: "每一張都代表我們緊緊相連的心，我愛妳鼻鼻，兩周年快樂❤️"
+        }
+    },
 ];
